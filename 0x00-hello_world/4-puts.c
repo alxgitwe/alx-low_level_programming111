e6255d2200ad2123d55c662329445384cb6bda7d
alxgitwe <el.najib15@gmail.com>
2023-06-15 16:20:49 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
-#!/bin/bash
-#include<stdio.h>
-int main()
+#include <stdio.h>
+/**
+ *main - void
+ *Description : first script with C
+ *Return : the value (0)
+ */
+int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
