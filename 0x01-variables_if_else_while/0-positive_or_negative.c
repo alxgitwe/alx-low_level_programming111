328042881712e5ca8fb3d2d18754c8ad1e37b535
alxgitwe <el.najib15@gmail.com>
2023-06-16 13:42:11 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -16,9 +16,9 @@ int main(void)
 	}
 	else if (n > 0)
 	{
-                printf("%d is positive\n", n);
-        }
-        else
+		printf("%d is positive\n", n);
+	}
+	else
 	{
 		printf("%d is zero\n", n);
 	}
