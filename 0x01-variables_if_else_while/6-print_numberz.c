241929d45cabb18a96d91f98db544cc060f28510
alxgitwe <el.najib15@gmail.com>
2023-06-16 16:38:11 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*main - prints all single digit numbers of base 10
+*Return: give a return 0 success
+*/
+int main(void)
+{
+	int digit;
+
+	for (digit = 0; digit < 10; digit)
+		putchar(digit);
+	putchar('\n');
+	return (0);
+}
