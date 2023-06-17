54f8c0259fcb437afc7bdb80855e2639cfb9ea93
alxgitwe <el.najib15@gmail.com>
2023-06-16 17:57:30 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,10 +10,10 @@ int main(void)
 
 	for (digit = 48; digit < 57; ++digit)
 	{
-		putchar(digit);
-		putchar(',');
+		putchar(digit + ',');
 		putchar(' ');
 	}
-	putchar(57, '\n');
+	putchar(57);
+	putchar('\n');
 	return (0);
 }
