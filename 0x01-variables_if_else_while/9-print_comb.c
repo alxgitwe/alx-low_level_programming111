6c52ca2fce635133e26855fc637245222c850913
alxgitwe <el.najib15@gmail.com>
2023-06-16 17:23:30 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*main - prints all single digit numbers of base 10
+*Return: give a return 0 success
+*/
+int main(void)
+{
+	int digit;
+
+	for (digit = 48; digit < 57; ++digit)
+		putchar(digit);
+		putchar(44);
+		putchar(39);
+	putchar(57);
+	return (0);
+}
