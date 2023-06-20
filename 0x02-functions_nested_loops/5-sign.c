897ce75e9343e3daa5cf0250c5f4eda6e46629da
alxgitwe <el.najib15@gmail.com>
2023-06-20 14:43:38 -0700

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * print_sign  - start of the program function that prints the sign of a number
+ *
+ * 5-sign.c : prints the sign of a number
+ *
+ * @n :a number
+ *
+ * Return: return the value 0
+ */
+
+int print_sign(int n);
+{
+	if (n > 0)
+	{
+		_putchar('43');
+		return (1);
+	}
+
+	else if (n < 0)
+	{
+		_putchar('45');
+		return (-1);
+	}
+
+	else
+	{
+		_putchar('48');
+		return (0);
+
+	}
+
+
+}
