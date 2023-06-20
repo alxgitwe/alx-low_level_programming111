a77bbc92551ca41985a09e485d6dcc2c668071c3
alxgitwe <el.najib15@gmail.com>
2023-06-20 13:00:39 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+
+
+
+#include "main.h"
+
+/**
+ * main - start of the program
+ *  _putchar - writes to stdout
+ * Return: return the value 0
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	return (0);
+}
