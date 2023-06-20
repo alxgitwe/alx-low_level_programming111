0cb0cf27aa5a9b40e07dff6fd002f62e2baed017
alxgitwe <el.najib15@gmail.com>
2023-06-20 13:38:28 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_alphabet - start of the program it prints the alphabet, in lowercase
+ *
+ * 1-alphabet.c :prints the alphabet, in lowercase, followed by a new line
+ *
+ * Return: return the value 0
+ */
+
+for (i = 0; i < 10; i++)
+{
+	void print_alphabet(void)
+	{
+		int a;
+
+		for (a = 97; a <= 122; a++)
+		{
+			_putchar(a);
+		}
+		_putchar('\n');
+
+
+	}
+
+}
