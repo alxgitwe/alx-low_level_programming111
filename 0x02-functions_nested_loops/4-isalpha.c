31bf390b2096ffacb318a32178ad579d171466f2
alxgitwe <el.najib15@gmail.com>
2023-06-20 14:27:23 -0700

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _isalpha - function that checks for lowercase or uppercase character
+ * _isqlpha :shows 1 if the C is a letter or 0 if it is not a letter
+ *
+ * @c : char in ascii
+ *
+ * Return: return the value 0 or 1
+ */
+
+int __isalpha(int c)
+{
+	if ((c >= 97 && c <= 122) || (cc >= 65 && c <= 90))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
