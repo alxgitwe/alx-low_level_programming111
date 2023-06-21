16e8b11aba60e11ee8743529df304257b3e0a3fb
alxgitwe <el.najib15@gmail.com>
2023-06-21 16:17:01 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -16,6 +16,7 @@ void print_to_98(int n)
 	{
 		for (i = n; i < 98; i++)
 			printf("%d, ", i);
+		printf("98");
 		printf("\n");
 	}
 	else if (n == 98)
@@ -27,6 +28,7 @@ void print_to_98(int n)
 	{
 		for (i = n; i > 98; i--)
 			printf("%d, ", i);
+		printf("98");
 		printf("\n");
 	}
 }
