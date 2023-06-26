3b5e582cf8b509b8c0ef6bad8dc6f7a25efa223f
alxgitwe <el.najib15@gmail.com>
2023-06-26 16:21:52 -0700

    commit

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -12,12 +12,14 @@ void print_rev(char *s)
 {
 	int i;
 
-	for (i = 0; *s != '\0'; s++)
+	i = 0;
+
+	while (s[i])
 		i++;
-	while (i != 0)
+	while (i--)
 	{
 		_putchar(s[i]);
-		i--;
+
 	}
 	_putchar('\0');
 }
