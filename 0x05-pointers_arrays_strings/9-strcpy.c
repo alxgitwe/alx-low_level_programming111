6906c08d1706ea1ba0699225e8aeaa152322a13f
alxgitwe <el.najib15@gmail.com>
2023-06-26 16:04:25 -0700

    9-strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * *_strcpy - function that copies the string pointed
+ *
+ * @dest : char
+ *
+ * @src : char
+ *
+ * Return: return value dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = -1;
+
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}
