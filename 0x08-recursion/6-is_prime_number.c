3b86eb865d5000f166ecc5329bae7413049c7fea
alxgitwe <el.najib15@gmail.com>
2023-07-06 13:27:02 -0700

    6-is_prime_number

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -12,9 +12,17 @@
  *
  */
 
+int modulo(n, k)
+{
+	if ((n % k) == 0)
+		return (0);
+	else
+		return (1);
+}
+
 int is_prime_number(int n)
 {
-	int k = 2;
+	unsigned int k = n - 1;
 
 	if (n == 2)
 		return (1);
@@ -22,13 +30,8 @@ int is_prime_number(int n)
 		return (0);
 	else if (n < 0)
 		return (is_prime_number(n * -1));
-	int modulo(n, k)
-	{
-		if (k == n)
-			break;
-		else if ((n % k) == 0)
-			return (1);
-		else
-			return (modulo(n, k + 1));
-	return (0);
+	else if (modulo(n, k) == 1)
+		return (1);
+	else if (modulo(n, k) == 0)
+		return (modulo(n, k - 1));
 }
