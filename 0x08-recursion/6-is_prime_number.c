3e643ed3cd38c545c70dee84ec705adb688c7c38
alxgitwe <el.najib15@gmail.com>
2023-07-06 13:05:18 -0700

    6-is_prime_number

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -4,6 +4,8 @@
  * is_prime_number - function that returns 1 if the input integer
  * is a prime number
  *
+ * modulo - function modulo
+ *
  * @n : int
  *
  * Return: return (1)
@@ -12,7 +14,7 @@
 
 int is_prime_number(int n)
 {
-	int k;
+	int k = 2;
 
 	if (n == 2)
 		return (1);
@@ -20,11 +22,13 @@ int is_prime_number(int n)
 		return (0);
 	else if (n < 0)
 		return (is_prime_number(n * -1));
-	for (k = 2; k < n; k++)
+	int modulo(n, k)
 	{
-		if (n % k == 0)
+		if (k == n)
 			break;
-		return (1);
-	}
+		else if ((n % k) == 0)
+			return (1);
+		else
+			return (modulo(n, k + 1));
 	return (0);
 }
