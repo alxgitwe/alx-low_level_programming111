cf86faec0090172f6c3b228c890f344ceedc197e
alxgitwe <el.najib15@gmail.com>
2023-07-07 16:46:34 -0700

    4-add

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - a program that adds positive numbers
+ *
+ * @argc : int
+ *
+ * @argv : char pointer
+ *
+ * Return: return (0) or return (1)
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	int j;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+	}
+	else if (argc > 1)
+	{
+		for (j = 1; j < argc; j++)
+		{
+			if (*argv[j] < '0' || *argv[j] > '9')
+				return (printf("Error\n"), 1);
+			i = i + atoi(argv[j]);
+		}
+	}
+	printf("%d\n", i);
+	return (0);
+
+}
