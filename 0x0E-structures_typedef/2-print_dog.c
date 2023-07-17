98ab8f0b02e4035e3803f57560f4a8ebe0e00887
alxgitwe <el.najib15@gmail.com>
2023-07-17 15:34:15 -0700

    m

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+
+/**
+ * print_dog - function that prints
+ *
+ * @d : struct
+ *
+ * Return: return
+ *
+ */
+
+void print_dog(struct dog *d)
+{
+	if ((d->name == NULL) || (d->age == NULL) || (d->owner == NULL))
+		printf("nil");
+	else if ((d->name == NULL))
+		printf("Name: (nil)");
+
+
+}
