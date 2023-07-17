3c5d689493bff292c54d5bab5066a36f4ef594d0
alxgitwe <el.najib15@gmail.com>
2023-07-17 13:55:50 -0700

    struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,26 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - struct dog
+ *
+ * @name : char
+ *
+ * @age : float
+ *
+ * @owner : char
+ *
+ * Return: return
+ *
+ */
+
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+
+} dog;
+
+#endif
+
