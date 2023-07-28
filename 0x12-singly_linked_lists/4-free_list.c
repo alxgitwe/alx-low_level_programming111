d273e99ebbb725b86b54443ca6861052b4cbaaaf
alxgitwe <el.najib15@gmail.com>
2023-07-28 11:33:27 -0700

    m

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_list - function that frees a list_t list
+ *
+ * @head : struct
+ *
+ * Return: return
+ *
+ */
+
+void free_list(list_t *head)
+{
+	while (head)
+	{
+		free(head);
+		head = head->next
+	}
+
+
+}
