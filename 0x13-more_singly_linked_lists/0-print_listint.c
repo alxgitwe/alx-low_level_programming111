2ad33f139cf3ee4cff0633a021fe3e581d180e08
alxgitwe <el.najib15@gmail.com>
2023-07-31 12:13:47 -0700

    m

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -11,13 +11,8 @@
 
 size_t print_listint(const listint_t *h)
 {
-	size_t a = 0;
+	int a = 0;
 
-	if (h == NULL)
-	{
-		printf("Error\n");
-		return (1);
-	}
 	while (h)
 	{
 		printf("%d\n", h->n);
