4e3d6a16e2d1d64dc3a4b08a62d1d1ed5b853429
alxgitwe <el.najib15@gmail.com>
2023-07-31 14:07:26 -0700

    m

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,28 @@
+i#include "lists.h"
+
+/**
+ * free_listint2 -  function that frees a
+ *
+ * @head : pointer
+ *
+ * Return: pointer
+ *
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *a;
+	listint_t *b;
+
+	a = *head;
+
+	while (a)
+	{
+		b = a->next;
+		free(a);
+		a = b;
+	} *head = NULL;
+
+
+
+}
