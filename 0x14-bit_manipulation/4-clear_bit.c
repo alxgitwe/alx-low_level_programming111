1a766b5535852ca52ab31eb967e0a8a3700108c0
alxgitwe <el.najib15@gmail.com>
2023-08-04 05:06:06 -0700

    m

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * clear_bit - function that sets the value of a bit to
+ *
+ * @n: int
+ *
+ * @index: index
+ *
+ * Return: retunr
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int a = sizeof(n) * 8;
+	unsigned long int b = *n & 1L;
+
+	if (a <= index)
+		return (-1);
+	if (b << index)
+		*n = *n ^ 1L << index;
+	return (1);
+
+
+}
