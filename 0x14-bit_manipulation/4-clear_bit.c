83214aac51989be59aee39008289cb65575f7a3c
alxgitwe <el.najib15@gmail.com>
2023-08-04 05:10:42 -0700

    m

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -13,11 +13,11 @@
 int clear_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned int a = sizeof(n) * 8;
-	unsigned long int b = *n & 1L;
+/*	unsigned long int b = *n & 1L; */
 
 	if (a >= index)
 		return (-1);
-	if (b << index)
+	if (*n & 1L << index)
 		*n = *n ^ 1L << index;
 	return (1);
 
