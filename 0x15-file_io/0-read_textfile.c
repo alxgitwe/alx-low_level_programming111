bc2d07f87cb6ebd9072975ecc35cad5eedb1f3d8
alxgitwe <el.najib15@gmail.com>
2023-08-07 13:16:00 -0700

    m

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,17 +11,17 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char buffer[8 * READ_BUF_SIZE];
+	char buffer[READ_BUF_SIZE * 8];
 	int a;
 	ssize_t b;
 
 	if (!letters || !filename)
 		return (0);
-	a = open(filename, 0_RDONLY);
+	a = open(filename, O_RDONLY);
 	if (a == -1)
 		return (0);
 	b = read(a, &buffer[0], letters);
-	b = write(STDOUT_FILENO, &BUFFER[0], b);
+	b = write(STDOUT_FILENO, &buffer[0], b);
 	close(a);
 	return (b);
 }
