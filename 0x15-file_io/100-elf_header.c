c5e535e512bd70e385741c273519192e4e5f6c38
alxgitwe <el.najib15@gmail.com>
2023-08-11 15:44:43 -0700

    m

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,134 @@
+#include <ELF.H>
+#include "main.h"
+
+/**
+ * _magic - print byts elf magic
+ * @hdr : elf header
+ */
+
+void _magic(Elf64_Ehdr hdr)
+{
+	int a;
+
+	printf("  Magic:   ");
+	for (a = 0; a < EI_NIDENT; a++)
+		printf("%2.2X%S", hdr.e_ident[a], a == EI_NIDENT - 1 ? "\n" : " ");
+}
+
+/**
+ * _class - class
+ * @hdr : elf header
+ */
+
+void _class(Elf64_Ehdr hdr)
+{
+	printf("  Class:                             ");
+	switch (hdr.e_ident[EI_CLASS])
+	{
+		case ELFCLASSNONE:
+			printf("NONE");
+			break;
+		case ELFCLASS32:
+			printf("ELF32");
+			break;
+		case ELFCLASS64:
+			printf("ELF32");
+			break;
+	}
+	printf("\n");
+}
+
+/**
+ * _data - print data elf
+ * @hdr : elf header
+ */
+
+void _data(Elf64_Ehdr hdr)
+{
+	printf("  Data:                              ");
+	switch (hdr.e_ident[EI_DATA])
+	{
+		case ELFDATANONE:
+			printf("NONE");
+			break;
+		case ELFDATA2LSB:
+			printf("2's complement, little endian");
+			break;
+		case ELFDATA2MSB:
+			printf("2's complement, big endian");
+			break;
+	}
+	printf("\n");
+}
+
+/**
+ * _version - print version elf
+ * @hdr : elf header
+ */
+
+void _version(Elf64_Ehdr hdr)
+{
+	printf("  Version:                           %d",hdr.e_ident[EI_VERSION]);
+	switch (hdr.e_ident[EI_VERSION])
+	{
+		case EV_NONE:
+			printf("%2s", "");
+			break;
+		case EV_CURRENT:
+			printf(" (current)");
+			break;
+	}
+	printf("\n");
+}
+
+void os_abi(hdr)
+{}
+void abi_version(hdr)
+{}
+void _type(hdr)
+{}
+void _epaddress(hdr)
+{}
+
+/**
+ * main - main
+ * @ac : argument
+ * @av : argument
+ * Return : return
+ */
+
+int main(int ac, char **av)
+{
+	Elf64_Ehdr hdr;
+	ssize_t a;
+	int b;
+
+	if (!(b != -1))
+                dprintf(STDERR_FILENO, "Can't open open the file : %s\n", av[1]), exit(98);
+	a = read(b, hdr, sizeof(hdr));
+	if (!(ac == 2))
+		dprintf(STDERR_FILENO, "Usage: elf_header elf_filename\n"), exit(98);
+	b =  open(av[1], O_RDONLY);
+	if (a != sizeof(hdr) || a < 1)
+		dprintf(STDERR_FILENO, "Can't open open the file : %s\n", av[1]), exit(98);
+
+
+	else
+		dprintf(STDERR_FILENO, "Not An Elf File:", av[1]), exit(98);
+	_magic(hdr);
+	_class(hdr);
+	_data(hdr);
+	_version(hdr);
+	os_abi(hdr);
+	abi_version(hdr);
+	_type(hdr);
+	_epaddress(hdr);
+
+	if (close(b))
+		dprintf(STDERR_FILENO, "Error Close File: %d\n", b), exit(98);
+	return (EXIT_SUCCESS);
+}
+
+
+
+
