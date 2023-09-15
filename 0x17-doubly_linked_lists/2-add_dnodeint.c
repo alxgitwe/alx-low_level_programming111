3d03b879426bbb2c7eeb27df6c38d4a7d0e8e032
alxgitwe <el.najib15@gmail.com>
2023-09-15 13:49:19 -0700

    m

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - function
+ * @head: adress
+ * @n: int
+ * Return: return
+ */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *a = mallow(sizeof(dlistint_t));
+
+	if (!head || a)
+		return (a ? free(a), NULL : NULL);
+	a->n = n;
+	a->prev = NULL;
+	if (!*head)
+	{
+		*head = a;
+		a->next = NULL
+	}
+	else
+	{
+		a->next = *head;
+		(*head)->prev = a;
+		*head = a;
+	}
+	return (a);
+}
+
+
+}
